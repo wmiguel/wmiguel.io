a05fa332b800aec65800dad265d92f08ff91f454
William Miguel <hello@w-miguel.com>
2020-05-29 21:36:41 -0700

    Switched to Handlebars.js and added half-star ratings.

--- js/projects/rogerebert/main.js
+++ js/projects/rogerebert/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+
   // Your web app's Firebase configuration
   var firebaseConfig = {
     apiKey: "AIzaSyBgEfLkADyRvqsQbZxrIYp4_Ef9Thy8b5k",
@@ -47,116 +48,87 @@ $(document).ready(function(){
       url: url+"&t="+filtered[0].Title,
       success: function(data){
 
+        console.log(filtered[0].Rating);
+
+        if (filtered[0].Rating === 4) {
+          var stars = `<i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i>`;
+        }
+        if (filtered[0].Rating === 3.5) {
+          var stars = `<i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star-half"></i>`;
+        }
+        if (filtered[0].Rating === 3) {
+          var stars = `<i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i>`;
+        }
+        if (filtered[0].Rating === 2.5) {
+          var stars = `<i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star-half"></i>`;
+        }
+        if (filtered[0].Rating === 2) {
+          var stars = `<i class="fa fa-star"></i> <i class="fa fa-star"></i>`;
+        }
+        if (filtered[0].Rating === 1.5) {
+          var stars = `<i class="fa fa-star"></i> <i class="fa fa-star-half"></i>`;
+        }
+        if (filtered[0].Rating === 1) {
+          var stars = `<i class="fa fa-star"></i>`;
+        }
+
+        var container = Handlebars.compile(`
+          <div class="container-fluid">
+
+            <div id="omdbInfo" class="row">
+
+              <div id="omdbImage" class="col-sm-3 order-lg-1 order-md-1 order-12">
+                <img id="omdbImgLink" src="{{getPoster}}">
+              </div>
+
+              <div id="omdbData" class="col-sm-9 order-lg-12 order-md-12 order-1">
+
+                <div id="omdbTitle" class="row">
+                  <div class="col">
+                    <h2>{{getTitle}}</h2>
+                    ${stars}
+                  </div>
+                </div>
+
+                <div id="omdbRYDG" class="row">
+                  <div class="col">{{getRated}} | {{getYear}} | {{getRuntime}} | {{getGenre}}</div>
+                </div>
+
+                <div id="omdbDirector" class="row">
+                  <div class="col">Directed by: {{getDirector}}</div>
+                </div>
+
+                <div id="omdbPlot" class="row">
+                  <div class="col">{{getPlot}}</div>
+                </div>
+
+              </div>
+
+            </div>
+
+            <div id="ebertReview" class="row">
+              <p>{{getEbertReview}}</p>
+              <a href="{{getEbertLink}}">Continue Reading on RogerEbert.com</a>
+            </div>
+
+          </div>`);
+
+
+
         // This replaces all the content in the default modal
-        var container = document.createElement('div');
-        container.className = 'container-fluid';
-        $('div.container-fluid').replaceWith(container);
+        $('div.container-fluid').replaceWith(container({
+          getPoster: data.Poster,
+          getTitle: data.Title,
+          getRated: data.Rated,
+          getYear: data.Year,
+          getRuntime: data.Runtime,
+          getGenre: data.Genre,
+          getPlot: data.Plot,
+          getDirector: data.Director,
+          getEbertReview: filtered[0].Review,
+          getEbertLink: filtered[0].Link,
+        }));
 
-        // Here we Insert the Poster, Title, Rating, Year, Runtime,
-        // Genre, Director, and Plot from OMDB into the modal
-
-        var omdbInfo = document.createElement('div');
-        omdbInfo.id = 'omdbInfo';
-        omdbInfo.className = 'row';
-        container.appendChild(omdbInfo);
-
-            var omdbImage = document.createElement('div');
-            omdbImage.id = 'omdbImage';
-            omdbImage.className = 'col-sm-3 order-lg-1 order-md-1 order-12';
-            omdbInfo.appendChild(omdbImage);
-
-                var omdbImgLink = document.createElement('img');
-                omdbImgLink.id = 'omdbImgLink';
-                omdbImgLink.setAttribute("src", data.Poster);
-                omdbImage.appendChild(omdbImgLink);
-
-            var omdbData = document.createElement('div');
-            omdbData.id = 'omdbData';
-            omdbData.className = 'col-sm-9 order-lg-12 order-md-12 order-1';
-            omdbInfo.appendChild(omdbData);
-
-                var omdbTitle = document.createElement('div');
-                omdbTitle.id = 'omdbTitle';
-                omdbTitle.className = 'row';
-                omdbData.appendChild(omdbTitle);
-
-                    var omdbColumnTitle = document.createElement('div');
-                    omdbColumnTitle.className = 'col';
-                    omdbTitle.appendChild(omdbColumnTitle);
-
-                        var omdbH2 = document.createElement('h2');
-                        dataTitle = document.createTextNode(data.Title);
-                        omdbH2.appendChild(dataTitle);
-                        omdbColumnTitle.appendChild(omdbH2);
-
-                        if (filtered[0].Rating === 4) {
-                          var stars = $('<i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i>');
-                          $(stars).appendTo(omdbColumnTitle);
-                        }
-                        if (filtered[0].Rating === 3) {
-                          var stars = $('<i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i>');
-                          $(stars).appendTo(omdbColumnTitle);
-                        }
-                        if (filtered[0].Rating === 2) {
-                          var stars = $('<i class="fa fa-star"></i> <i class="fa fa-star"></i>');
-                          $(stars).appendTo(omdbColumnTitle);
-                        }
-                        if (filtered[0].Rating === 1) {
-                          var stars = $('<i class="fa fa-star"></i>');
-                          $(stars).appendTo(omdbColumnTitle);
-                        }
-
-                var omdbRYDG = document.createElement('div');
-                omdbRYDG.id = 'omdbRYDG';
-                omdbRYDG.className = 'row';
-                omdbData.appendChild(omdbRYDG);
-
-                    var omdbColumnRYDG = document.createElement('div');
-                    omdbColumnRYDG.className = 'col';
-                    ryrgText = document.createTextNode( `${data.Rated} | ${data.Year} | ${data.Runtime} | ${data.Genre}` );
-                    omdbColumnRYDG.appendChild(ryrgText);
-                    omdbRYDG.appendChild(omdbColumnRYDG);
-
-                var omdbDirector = document.createElement('div');
-                omdbDirector.id = 'omdbDirector';
-                omdbDirector.className = 'row';
-                omdbData.appendChild(omdbDirector);
-
-                    var omdbColumnDirector = document.createElement('div');
-                    omdbColumnDirector.className = 'col';
-                    directorText = document.createTextNode( `Directed by: ${data.Director}` );
-                    omdbColumnDirector.appendChild(directorText);
-                    omdbDirector.appendChild(omdbColumnDirector);
-
-                var omdbPlot = document.createElement('div');
-                omdbPlot.id = 'omdbPlot';
-                omdbPlot.className = 'row';
-                omdbData.appendChild(omdbPlot);
-
-                    var omdbColumnPlot = document.createElement('div');
-                    omdbColumnPlot.className = 'col';
-                    plotText = document.createTextNode(data.Plot);
-                    omdbColumnPlot.appendChild(plotText);
-                    omdbPlot.appendChild(omdbColumnPlot);
-
-        // Here we insert the Review, and Url Link
-        // into the Modal from Our review.js
-
-        var ebertReview = document.createElement('div');
-        ebertReview.id = 'ebertReview';
-        ebertReview.className = 'row';
-        container.appendChild(ebertReview);
-
-            var ebertReviewP = document.createElement('p');
-            reviewArray = document.createTextNode(filtered[0].Review);
-            ebertReviewP.appendChild(reviewArray);
-            ebertReview.appendChild(ebertReviewP);
-
-            var ebertLinkA = document.createElement('a');
-            ebertLinkA.setAttribute('href', filtered[0].Link)
-            linkArray = document.createTextNode('Continue Reading on RogerEbert.com');
-            ebertLinkA.appendChild(linkArray);
-            ebertReview.appendChild(ebertLinkA);
 
           }
     })
