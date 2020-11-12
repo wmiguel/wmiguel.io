3cec563d575164ece14cc99efdadd28a013af00a
William Miguel <hello@w-miguel.com>
2020-11-11 22:23:37 -0800

    Reduced size of js file using Handlebars.js

--- js/projects/69-love-songs/main.js
+++ js/projects/69-love-songs/main.js
@@ -1,306 +1,230 @@
-var track, title, artist, album, year, cover, lyrics;
-var volumeOne = document.getElementById("vol-01");
-var volumeTwo = document.getElementById("vol-02");
-var volumeThree = document.getElementById("vol-03");
-var imgAlbumLink = document.getElementById("album-link");
-
-var lyricsTitle = document.getElementById("trackTitle");
-var lyricsNumberVolume = document.getElementById("trackNumberVolume");
-var lyricsArtist = document.getElementById("trackArtist");
-var lyricsAlbum = document.getElementById("trackAlbum");
-
-var trackCover = document.getElementById("trackCover");
-var lyricsText = document.getElementById("track-fullLyrics");
 var albumDeactivate = document.getElementById('album-content');
 var lyricsActivate = document.getElementById('lyrics-content');
 var mainContent = document.getElementById('main-content');
+
 var previous = document.getElementById("previous");
 var next = document.getElementById("next");
 var previousMobile = document.getElementById("previous-mobile");
 var nextMobile = document.getElementById("next-mobile");
 
-var albumCover = loveSongs[0].AlbumCover;
-imgAlbumLink.src = "https://wmiguel.io/" + albumCover; // "file:///Users/wmiguel/Sites/wmiguel.io/" + albumCover;
+function previousOpacityFull() {
+  previous.style.opacity = 1;
+  previousMobile.style.opacity = 1;
+}
 
-for (var songs = 1; songs <= 23; songs++) {
-  var trackNumber = loveSongs[songs].TrackNumber;
-  var trackTitle = loveSongs[songs].Title;
-  var trackTitles = Handlebars.compile(`
-    <div id="album-track" onclick="trackSelected(${songs});">
-      <p>${trackNumber}</p>
-      <p>${trackTitle}</p>
-    </div>`);
-  $('#album-tracks').append(trackTitles());
-};
+function nextOpacityFull() {
+  next.style.opacity = 1;
+  nextMobile.style.opacity = 1;
+}
 
-document.addEventListener('keydown', escapeTrack);
+function fullOpacity() {
+  previous.style.opacity = 1;
+  next.style.opacity = 1;
+  previousMobile.style.opacity = 1;
+  nextMobile.style.opacity = 1;
+}
+
+function contentSwitch() {
+  mainContent.style.display = 'none';
+  lyricsActivate.style.visibility = 'hidden';
+  lyricsActivate.style.opacity = '0';
+  lyricsActivate.style.display = 'none';
+  albumDeactivate.style.visibility = 'visible';
+  albumDeactivate.style.opacity = '1';
+  albumDeactivate.style.display = 'block';
+}
+
+$(document).ready(function() {
+  for (var songs = 1; songs <= 23; songs++) {
+    var trackTitles = Handlebars.compile(`
+      <div id="album-track" onclick="trackSelected(${songs});">
+        <p>${loveSongs[songs].TrackNumber}</p>
+        <p>${loveSongs[songs].Title}</p>
+      </div>`);
+    $('#album-tracks').append(trackTitles());
+  };
+});
 
 function volume(vol) {
   if (vol == 1) {
-    volumeOne.classList.add("active");
-    if ($('#vol-02').hasClass('active') || $('#vol-03').hasClass('active')) {
-      $('#vol-02').removeClass('active');
-      $('#vol-03').removeClass('active');
-    }
+    $('#vol-01').addClass('active');
+    $("#vol-02,#vol-03").removeClass('active');
     $("#album-tracks").empty();
     for (var songs = 1; songs <= 23; songs++) {
-      var trackNumber = loveSongs[songs].TrackNumber;
-      var trackTitle = loveSongs[songs].Title;
       var trackTitles = Handlebars.compile(`
         <div id="album-track" onclick="trackSelected(${songs});">
-          <p>${trackNumber}</p>
-          <p>${trackTitle}</p>
+          <p>${loveSongs[songs].TrackNumber}</p>
+          <p>${loveSongs[songs].Title}</p>
         </div>`);
-      $('#album-tracks').append(trackTitles());
+      $('#album-tracks').append(trackTitles(songs));
+      console.log(trackTitles());
     };
   }
   if (vol == 2) {
-    volumeTwo.classList.add("active");
-    if ($('#vol-01').hasClass('active') || $('#vol-03').hasClass('active')) {
-      $('#vol-01').removeClass('active');
-      $('#vol-03').removeClass('active');
-    }
+    $('#vol-02').addClass('active');
+    $("#vol-01,#vol-03").removeClass('active');
     $("#album-tracks").empty();
     for (var songs = 24; songs <= 46; songs++) {
-      var trackNumber = loveSongs[songs].TrackNumber;
-      var trackTitle = loveSongs[songs].Title;
       var trackTitles = Handlebars.compile(`
         <div id="album-track" onclick="trackSelected(${songs});">
-          <p>${trackNumber}</p>
-          <p>${trackTitle}</p>
+          <p>${loveSongs[songs].TrackNumber}</p>
+          <p>${loveSongs[songs].Title}</p>
         </div>`);
       $('#album-tracks').append(trackTitles());
     };
   }
   if (vol == 3) {
-    volumeThree.classList.add("active");
-    if ($('#vol-01').hasClass('active') || $('#vol-02').hasClass('active')) {
-      $('#vol-01').removeClass('active');
-      $('#vol-02').removeClass('active');
-    }
+    $('#vol-03').addClass('active');
+    $("#vol-01,#vol-02").removeClass('active');
     $("#album-tracks").empty();
     for (var songs = 47; songs <= 69; songs++) {
-      var trackNumber = loveSongs[songs].TrackNumber;
-      var trackTitle = loveSongs[songs].Title;
       var trackTitles = Handlebars.compile(`
         <div id="album-track" onclick="trackSelected(${songs});">
-          <p>${trackNumber}</p>
-          <p>${trackTitle}</p>
+          <p>${loveSongs[songs].TrackNumber}</p>
+          <p>${loveSongs[songs].Title}</p>
         </div>`);
       $('#album-tracks').append(trackTitles());
     };
   }
+
 }
+
 function trackSelected(i) {
-  track = loveSongs[i].TrackNumber;
-  trackVolume = loveSongs[i].Volume;
-  title = loveSongs[i].Title;
-  artist = loveSongs[i].Artist;
-  album = loveSongs[i].Album;
-  year = loveSongs[i].Year;
-  cover = loveSongs[i].AlbumCover;
-  lyrics = loveSongs[i].Lyrics;
-
-  lyricsNumberVolume.textContent = `Track ${track} | Volume ${trackVolume}`
-  lyricsTitle.textContent = title;
-  lyricsArtist.textContent = `${artist}`;
-  lyricsAlbum.textContent = `${album} (${year})`;
-
-  trackCover.src = "https://wmiguel.io/" + cover; // "file:///Users/wmiguel/Sites/wmiguel.io/" + cover;
-  lyricsText.innerHTML = lyrics;
-
+  var trackLyrics = Handlebars.compile(`
+    <div id="track-information">
+      <img id="trackCover" src="https://wmiguel.io/${loveSongs[i].AlbumCover}">
+      <h3 id="trackNumberVolume">Track ${loveSongs[i].TrackNumber} | Volume ${loveSongs[i].Volume}</h3>
+      <h1 id="trackTitle">${loveSongs[i].Title}</h1>
+      <h2 id="trackArtist">${loveSongs[i].Artist}</h2>
+      <h2 id="trackAlbum">${loveSongs[i].Album} (${loveSongs[i].Year})</h2>
+    </div>
+    <div id="track-fullLyrics">${loveSongs[i].Lyrics}</div>`);
+  $('#track-lyrics').append(trackLyrics());
   lyricsActivate.style.visibility = 'visible';
   lyricsActivate.style.opacity = '1';
   lyricsActivate.style.display = 'block';
-
   mainContent.style.display = 'grid';
-
-  albumDeactivate.style.visibility = 'hidden';
+  albumDeactivate.style.visibility = 'hidden';          
   albumDeactivate.style.opacity = '0';
   albumDeactivate.style.display = 'none';
-
   previous.innerHTML = "<button onclick='previousTrack(" + i + ");'>&#10094;</button>";
   previousMobile.innerHTML = "<button onclick='previousTrack(" + i + ");'>&#10094;</button>";
   next.innerHTML = "<button onclick='nextTrack(" + i + ");'>&#10095;</button>";
   nextMobile.innerHTML = "<button onclick='nextTrack(" + i + ");'>&#10095;</button>";
 
   document.onkeydown = function(event) {
-    var e = event || window.event;
-    var lyricsNumberVolume = document.getElementById("trackNumberVolume");
-    var lyricsTitle = document.getElementById("trackTitle");
-    var lyricsArtist = document.getElementById("trackArtist");
-    var lyricsAlbum = document.getElementById("trackAlbum");
 
-    var trackCover = document.getElementById("trackCover");
-    var lyricsText = document.getElementById("track-fullLyrics");
+    var e = event || window.event;
     var previous = document.getElementById("previous");
     var previousMobile = document.getElementById("previous-mobile");
     var next = document.getElementById("next");
     var nextMobile = document.getElementById("next-mobile");
 
-    if (e.keyCode == '39') { // Right Key
-      if (i == 23) {
+    if (e.keyCode == 39) { // Right Key
+      if (i == 23 || i == 46 || i == 69) {
         return;
-      }
-      if (i == 46) {
-        return;
-      }
-      if (i == 69) {
-        return;
-      }
-      else {
+      } else {
         nextNum = i += 1;
-        lyricsNumberVolume.textContent = `Track ${loveSongs[i].TrackNumber} | Volume ${loveSongs[i].Volume}`;
-        lyricsTitle.textContent = loveSongs[i].Title;
-        lyricsArtist.textContent = loveSongs[i].Artist;
-        lyricsAlbum.textContent = `${loveSongs[i].Album} (${loveSongs[i].Year})`;
+        $("#track-lyrics").empty();
+        var trackLyrics = Handlebars.compile(`
+          <div id="track-information">
+            <img id="trackCover" src="https://wmiguel.io/${loveSongs[i].AlbumCover}">
+            <h3 id="trackNumberVolume">Track ${loveSongs[i].TrackNumber} | Volume ${loveSongs[i].Volume}</h3>
+            <h1 id="trackTitle">${loveSongs[i].Title}</h1>
+            <h2 id="trackArtist">${loveSongs[i].Artist}</h2>
+            <h2 id="trackAlbum">${loveSongs[i].Album} (${loveSongs[i].Year})</h2>
+          </div>
+          <div id="track-fullLyrics">${loveSongs[i].Lyrics}</div>`);
+        $('#track-lyrics').append(trackLyrics());
 
-        trackCover.src = "https://wmiguel.io/" + loveSongs[i].AlbumCover; // "file:///Users/wmiguel/Sites/wmiguel.io/" + loveSongs[i].AlbumCover;
-        lyricsText.innerHTML = loveSongs[i].Lyrics;
         next.innerHTML = "<button onclick='nextTrack(" + i + ");'>&#10095;</button>";
-        nextMobile.innerHTML = "<button onclick='nextTrack(" + i + ");'>&#10095;</button>";
         previous.innerHTML = "<button onclick='previousTrack(" + i + ");'>&#10094;</button>";
+        nextMobile.innerHTML = "<button onclick='nextTrack(" + i + ");'>&#10095;</button>";
         previousMobile.innerHTML = "<button onclick='previousTrack(" + i + ");'>&#10094;</button>";
-        previous.style.opacity = 1;
-        if (i == 23) {
-          next.style.opacity = .5;
-          nextMobile.style.opacity = .5;
-        }
-        if (i == 47) {
-          next.style.opacity = .5;
-          nextMobile.style.opacity = .5;
-        }
-        if (i == 69) {
+
+        previousOpacityFull();
+
+        if (i == 23 || i == 46 || i == 69) {
           next.style.opacity = .5;
           nextMobile.style.opacity = .5;
         }
       }
     }
-    if (e.keyCode == '37') { // Left Key
-      if (i == 1) {
+
+    if (e.keyCode == 37) { // Left Key
+      if (i == 1 || i == 24 || i == 47) {
         return;
-      }
-      if (i == 24) {
-        return;
-      }
-      if (i == 47) {
-        return;
-      }
-      else {
+      } else {
         nextNum = i -= 1;
-        lyricsNumberVolume.textContent = `Track ${loveSongs[i].TrackNumber} | Volume ${loveSongs[i].Volume}`;
-        lyricsTitle.textContent = loveSongs[i].Title;
-        lyricsArtist.textContent = loveSongs[i].Artist;
-        lyricsAlbum.textContent = `${loveSongs[i].Album} (${loveSongs[i].Year})`;
+        $("#track-lyrics").empty();
+        var trackLyrics = Handlebars.compile(`
+          <div id="track-information">
+            <img id="trackCover" src="https://wmiguel.io/${loveSongs[i].AlbumCover}">
+            <h3 id="trackNumberVolume">Track ${loveSongs[i].TrackNumber} | Volume ${loveSongs[i].Volume}</h3>
+            <h1 id="trackTitle">${loveSongs[i].Title}</h1>
+            <h2 id="trackArtist">${loveSongs[i].Artist}</h2>
+            <h2 id="trackAlbum">${loveSongs[i].Album} (${loveSongs[i].Year})</h2>
+          </div>
+          <div id="track-fullLyrics">${loveSongs[i].Lyrics}</div>`);
+        $('#track-lyrics').append(trackLyrics());
 
-        trackCover.src = "https://wmiguel.io/" + loveSongs[i].AlbumCover; // "file:///Users/wmiguel/Sites/wmiguel.io/" + loveSongs[i].AlbumCover;
-        lyricsText.innerHTML = loveSongs[i].Lyrics;
         next.innerHTML = "<button onclick='nextTrack(" + i + ");'>&#10095;</button>";
-        nextMobile.innerHTML = "<button onclick='nextTrack(" + i + ");'>&#10095;</button>";
-        next.style.opacity = 1;
-        nextMobile.style.opacity = 1;
         previous.innerHTML = "<button onclick='previousTrack(" + i + ");'>&#10094;</button>";
+        nextMobile.innerHTML = "<button onclick='nextTrack(" + i + ");'>&#10095;</button>";
         previousMobile.innerHTML = "<button onclick='previousTrack(" + i + ");'>&#10094;</button>";
-        if (i == 1) {
-          previous.style.opacity = .5;
-          previousMobile.style.opacity = .5;
-        }
-        if (i == 24) {
-          previous.style.opacity = .5;
-          previousMobile.style.opacity = .5;
-        }
-        if (i == 47) {
+
+        nextOpacityFull()
+
+        if (i == 1 || i == 24 || i == 47) {
           previous.style.opacity = .5;
           previousMobile.style.opacity = .5;
         }
       }
     }
+
   }
 }
+
 function nextTrack(num) {
-  console.log("next");
-  if (num == 23) {
+  if (num == 23 || num == 46 || num == 69) {
     return;
-  }
-  if (num == 46) {
-    return;
-  }
-  if (num == 69) {
-    return;
-  }
-  else {
+  } else {
     var newNum = num += 1;
+    $("#track-lyrics").empty();
     trackSelected(newNum);
     previous.style.opacity = 1;
     previousMobile.style.opacity = 1;
-    if (num == 23) {
-      next.style.opacity = .5;
-      nextMobile.style.opacity = .5;
-    }
-    if (num == 46) {
-      next.style.opacity = .5;
-      nextMobile.style.opacity = .5;
-    }
-    if (num == 69) {
+    if (num == 23 || num == 46 || num == 69) {
       next.style.opacity = .5;
       nextMobile.style.opacity = .5;
     }
   }
 };
+
 function previousTrack(num) {
-  console.log("previous");
-  if (num == 1) {
+  if (num == 1 || num == 24 || num == 47) {
     return;
-  }
-  if (num == 24) {
-    return;
-  }
-  if (num == 47) {
-    return;
-  }
-  else {
+  } else {
     var newNum = num -= 1;
+    $("#track-lyrics").empty();
     trackSelected(newNum);
     next.style.opacity = 1;
     nextMobile.style.opacity = 1;
-    if (num == 1) {
-      previous.style.opacity = .5;
-      previousMobile.style.opacity = .5;
-    }
-    if (num == 24) {
-      previous.style.opacity = .5;
-      previousMobile.style.opacity = .5;
-    }
-    if (num == 47) {
+    if (num == 1 || num == 24 || num == 47) {
       previous.style.opacity = .5;
       previousMobile.style.opacity = .5;
     }
   }
 };
-function goHome() {
-  lyricsActivate.style.visibility = 'hidden';
-  lyricsActivate.style.opacity = '0';
-  lyricsActivate.style.display = 'none';
-
-  mainContent.style.display = 'none';
-
-  albumDeactivate.style.visibility = 'visible';
-  albumDeactivate.style.opacity = '1';
-  albumDeactivate.style.display = 'block';
+document.addEventListener('keydown', escapeTrack);
+function goHome(esc) {
+  fullOpacity();
+  $("#track-lyrics").empty();
+  contentSwitch();
 };
+
 function escapeTrack(esc) {
-  if (esc.code == 'Escape') {
-    var lyricsActivate = document.getElementById('lyrics-content');
-    var albumDeactivate = document.getElementById('album-content');
-    var mainContent = document.getElementById('main-content');
-    lyricsActivate.style.visibility = 'hidden';
-    lyricsActivate.style.opacity = '0';
-    lyricsActivate.style.display = 'none';
-
-    mainContent.style.display = 'none';
-
-    albumDeactivate.style.visibility = 'visible';
-    albumDeactivate.style.opacity = '1';
-    albumDeactivate.style.display = 'block';
+  if (esc.keyCode == 27) {
+    goHome(esc);
   }
 }
